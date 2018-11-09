8ce3212a849da76d030e83404e42c3765c8f0612
jerry <jm90_yang@163.com>
2018-11-09 13:34:56 +0800

    优化目录结构,

---
+++ services/user-service/src/main/java/com/house/userservice/config/Demo.java
@@ -0,0 +1,10 @@
+package com.house.userservice.config;
+
+/**
+ * @ Author     ：jmyang
+ * @ Date       ：Created in 2018/11/9
+ * @ Description：一个无用demo,为了使目录层次结构化
+ * @ throws
+ */
+public class Demo {
+}
---
+++ services/user-service/src/main/java/com/house/userservice/config/http/HttpClientProperties.java
@@ -0,0 +1,52 @@
+package com.house.userservice.config.http;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@ConfigurationProperties(prefix="spring.httpclient",ignoreInvalidFields = true,ignoreUnknownFields = false)
+@PropertySource("classpath:/httpClient.properties")
+@Configuration
+public class HttpClientProperties {
+	
+	private Integer connectTimeOut;
+	
+	private Integer socketTimeOut;
+
+	private String agent;
+	private Integer maxConnPerRoute;
+	private Integer maxConnTotaol;
+	public Integer getConnectTimeOut() {
+		return connectTimeOut;
+	}
+	public void setConnectTimeOut(Integer connectTimeOut) {
+		this.connectTimeOut = connectTimeOut;
+	}
+	public Integer getSocketTimeOut() {
+		return socketTimeOut;
+	}
+	public void setSocketTimeOut(Integer socketTimeOut) {
+		this.socketTimeOut = socketTimeOut;
+	}
+	public String getAgent() {
+		return agent;
+	}
+	public void setAgent(String agent) {
+		this.agent = agent;
+	}
+	public Integer getMaxConnPerRoute() {
+		return maxConnPerRoute;
+	}
+	public void setMaxConnPerRoute(Integer maxConnPerRoute) {
+		this.maxConnPerRoute = maxConnPerRoute;
+	}
+	public Integer getMaxConnTotaol() {
+		return maxConnTotaol;
+	}
+	public void setMaxConnTotaol(Integer maxConnTotaol) {
+		this.maxConnTotaol = maxConnTotaol;
+	}
+	
+	
+	
+}
