1f99ac5f9fb2420c7b34b618940d68606f29fcf6
jerry <jm90_yang@163.com>
2018-11-09 14:56:56 +0800

    添加mybatis配置文件

--- services/user-service/src/main/java/com/house/userservice/UserServiceApplication.java
+++ services/user-service/src/main/java/com/house/userservice/UserServiceApplication.java
@@ -1,11 +1,13 @@
 package com.house.userservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan(basePackages = {"com.house.userservice.mapper"})//mapper 层扫描
 public class UserServiceApplication {
 
     public static void main(String[] args) {
