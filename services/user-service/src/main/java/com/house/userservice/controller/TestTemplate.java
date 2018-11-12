1b669d70e62c24aa3a9ced3e59ecadd513b6ced2
jerry <jm90_yang@163.com>
2018-11-12 09:37:56 +0800

    redis 本地配置、测试

--- services/user-service/src/main/java/com/house/userservice/controller/TestTemplate.java
+++ services/user-service/src/main/java/com/house/userservice/controller/TestTemplate.java
@@ -1,5 +1,7 @@
 package com.house.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestTemplate {
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @RequestMapping("/getUser")
     public String test(Integer id){
+        redisTemplate.opsForValue().set("hei", "hello");
         System.out.println(id+":从api-server传递过来的id值");
         return "test-user";
     }
