521d42b278a163e9b17323fe35ffde98a1c54146
jerry <jm90_yang@163.com>
2018-11-14 09:42:52 +0800

    增加swagger2 测试配置

--- services/user-service/src/main/java/com/house/userservice/UserServiceApplication.java
+++ services/user-service/src/main/java/com/house/userservice/UserServiceApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan(basePackages = {"com.house.userservice.mapper"})//mapper 层扫描
+@EnableSwagger2
 public class UserServiceApplication {
 
     public static void main(String[] args) {
