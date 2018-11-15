762de0c4f982a820426a43fc11996909f79c0e05
jerry <jm90_yang@163.com>
2018-11-15 11:24:07 +0800

    数据库驱动配置,http配置

--- services/house-service/src/main/java/com/house/houseservice/HouseServiceApplication.java
+++ services/house-service/src/main/java/com/house/houseservice/HouseServiceApplication.java
@@ -1,9 +1,15 @@
 package com.house.houseservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan(basePackages = {"com.house.houseservice.mapper"})//mapper 层扫描
+@EnableSwagger2
 public class HouseServiceApplication {
 
     public static void main(String[] args) {
