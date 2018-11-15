45cd01a470883d7347936f425b7323ece6df0adc
jerry <jm90_yang@163.com>
2018-11-15 15:00:24 +0800

    增加要监控的模块的配置信息

--- services/api-gateway/src/main/java/com/house/apigateway/ApiGatewayApplication.java
+++ services/api-gateway/src/main/java/com/house/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,12 @@ package com.house.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
---
+++ services/api-gateway/src/main/java/com/house/apigateway/config/HyConfig.java
@@ -0,0 +1,26 @@
+package com.house.apigateway.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ Author     ：jmyang
+ * @ Date       ：Created in 2018/11/15
+ * @ Description：此配置适合spring boot 2.0 以上
+ * @ throws
+ */
+@Configuration
+public class HyConfig {
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
